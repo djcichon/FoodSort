b98c53cc243f5da71e784dd88a138f375143cde6
Doug Cichon <djcichon@gmail.com>
2016-10-01 12:06:22 -0400

    Fix :through relationships

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,14 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+
+	has_many :dishes
+	has_many :recipe_products
+
 	has_many :grocery_trips, through: :dishes
 	has_many :products, through: :recipe_products
 
+	accepts_nested_attributes_for :recipe_products
+	#TODO: May not want this, could lead to product names changing accidentally.  Not sure yet.
+	accepts_nested_attributes_for :products
 	validates_uniqueness_of :name, :scope => :user_id
 end
