3d10838710f8b1de61d6d457b89ef52572d88683
Doug Cichon <djcichon@gmail.com>
2016-10-08 12:56:57 -0400

    Populate grocery list based on meals

--- app/controllers/grocery_trips_controller.rb
+++ app/controllers/grocery_trips_controller.rb
@@ -6,4 +6,12 @@ class GroceryTripsController < ApplicationController
 
   def edit
   end
+
+	# Expects params[:recipe_id], returns a list of name/order pairs for it's ingredients.
+	def get_ingredients
+		recipe = Recipe.find(params[:recipe_id].to_i)
+
+		#TODO: Handle recipe not found
+		render json: recipe.products.map { |product| { name: product.name, order: product.order } }
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
 	post 'products' => 'products#update'
+	get 'grocery_trips/get_ingredients' => 'grocery_trips#get_ingredients'
 	resources :products, only: [:index]
 	resources :recipes, only: [:new, :create, :edit, :update]
 	resources :grocery_trips, only: [:new, :create, :edit, :update]
