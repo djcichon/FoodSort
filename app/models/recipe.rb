1ae0893d797769eae3ac84d853f722504b35212e
Doug Cichon <djcichon@gmail.com>
2016-10-11 22:28:55 -0400

    Handle 2+ of new and same ingredients in a recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,4 +11,24 @@ class Recipe < ApplicationRecord
 	#TODO: May not want this, could lead to product names changing accidentally.  Not sure yet.
 	accepts_nested_attributes_for :products
 	validates_uniqueness_of :name, :scope => :user_id
+
+	before_save :combine_duplicate_products
+
+	private
+		def combine_duplicate_products
+			# Maps product name to a unique product
+			product_map = Hash.new
+
+			recipe_products.each do |rp|
+
+				# If the product name has been seen already, use the first product that was seen
+				if product_map.include?(rp.name)
+					rp.product = product_map[rp.name]
+
+				# If the product name has not been seen yet, remember the unique product to use
+				else
+					product_map[rp.name] = rp.product
+				end
+			end
+		end
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -12,6 +12,24 @@ class RecipeTest < ActiveSupport::TestCase
 			# Shouldn't be created since it already exists
 			@user.recipes.create(name: "yummy")
 		end
+	end
 
+	test "duplicate products by name should point to same product" do
+		# Create a recipe with two of the same product
+		recipe = @user.recipes.new(name: "yummy")
+		recipe.recipe_products.new(name: "kale")
+		recipe.recipe_products.new(name: "kale")
+
+		assert recipe.valid?
+
+		# Only one Product should be created when saving
+		assert_difference 'Product.count', 1 do
+			recipe.save
+		end
+
+    # After saving, both should point to the same product
+		assert_equal 2, recipe.products.size
+		assert_equal 2, recipe.recipe_products.size
+		assert_equal recipe.products[0], recipe.products[1]
 	end
 end
