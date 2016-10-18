4a86d93f291ef46e5c0b1171394cd0c3cc392c65
Doug Cichon <djcichon@gmail.com>
2016-10-18 19:04:57 -0400

    Add tests for home controller

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+	include Devise::Test::IntegrationHelpers
+
+	test "home when logged in" do
+		@user = users(:doug)
+		sign_in @user
+
+		get root_url
+    assert_response :success
+		assert_select 'a', 'New Recipe'
+	end
+
+	test "home when not logged in" do
+		get root_url
+    assert_response :success
+		assert_select 'a', 'Create account'
+	end
 end
