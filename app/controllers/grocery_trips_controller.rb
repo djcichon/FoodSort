f9489b883a30ae1d4367b62820cfd0de9b524bbc
Doug Cichon <djcichon@gmail.com>
2016-10-18 22:20:01 -0400

    Handle recipe not found

--- app/controllers/grocery_trips_controller.rb
+++ app/controllers/grocery_trips_controller.rb
@@ -4,14 +4,16 @@ class GroceryTripsController < ApplicationController
 		@recipes = current_user.recipes
   end
 
-  def edit
-  end
-
 	# Expects params[:recipe_id], returns a list of name/order pairs for it's ingredients.
 	def get_ingredients
-		recipe = Recipe.find(params[:recipe_id].to_i)
+		recipe_id = params[:recipe_id].to_i
+		recipe = Recipe.find_by(id: recipe_id)
 
-		#TODO: Handle recipe not found
-		render json: recipe.products.map { |product| { name: product.name, order: product.order } }
+		if recipe
+			render json: recipe.products.map { |product| { name: product.name, order: product.order } }
+		else
+			# If the recipe is not found, gracefully return no ingredients
+			render json: []
+		end
 	end
 end
