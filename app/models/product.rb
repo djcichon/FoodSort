a570805d73af6a97aa70a2bb2114f1baf50034a4
Doug Cichon <djcichon@gmail.com>
2016-10-18 21:34:59 -0400

    Do not allow negative orders

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
 
 	has_many :recipe_products
 	has_many :recipes, through: :recipe_products
+
+	validates :order, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
 end
