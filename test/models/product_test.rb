dbc7faa712891981f38cb0dee674699d68ecaac6
Doug Cichon <djcichon@gmail.com>
2016-10-20 07:58:37 -0400

    Add product test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "order cannot be negative" do
+    product = Product.new(order: nil)
+    assert product.valid?, "Expected product to be valid"
+
+    product.order = -1
+    assert_not product.valid?, "Expected product to be invalid"
+
+    product.order = 1
+    assert product.valid?, "Expected product to be valid"
+  end
 end
